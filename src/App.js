59c889f5672d9b109706f10019ddde5f2b090bf7
cimendes <iines.mendes@gmail.com>
2020-04-07 12:20:06 +0100

    media queries with Radium - StyleRoot component

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ export default app;
 //Using react classes
 import React, { Component } from 'react';
 import './App.css';
-import Radium from "radium";
+import Radium, { StyleRoot } from "radium";
 import Person from './Person/Person';
 
 class App extends Component {
@@ -148,6 +148,8 @@ class App extends Component {
         }
 
         return (
+            // from radium, necessary for media queries!
+            <StyleRoot>
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p className={classes.join(' ')}>This is really working!</p>
@@ -158,6 +160,7 @@ class App extends Component {
                 </button>
                 {persons}
             </div>
+            </StyleRoot>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,14 @@ import './Person.css';
 import Radium from "radium";
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+
     return (
-        <div className={"Person"}>
+        <div className={"Person"} style={style}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -12,4 +18,5 @@ const person = (props) => {
     )
 };
 
+// necessary for media queries
 export default Radium(person);
